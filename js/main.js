45e4af3539ee579e51eee637fabefa07e837d4e2
trwhitaker.io <thomaswhitaker@protonmail.com>
2017-10-10 21:57:06 +1100

    Initial Import 10/10/17
    Initial setup on GitHub.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+
+document.getElementById('menuBtn').addEventListener('click', function (  ) {
+  // OPEN MENU
+  if (document.getElementById('menuSlide').classList.contains('menuMobile' && 'slideMenuOpen')) {
+   document.getElementById('menuSlide').classList.add('menuMobileOpen');
+   document.getElementById('x').classList.add('xClose');
+   document.getElementById('y').classList.add('yClose');
+   document.getElementById('z').classList.add('zClose');
+   document.getElementById('background').classList.add('backgroundOn');
+   document.getElementById('menuSlide').classList.remove('slideMenuOpen');
+   console.log("That menu open is smooth, am I right?");
+ } else {
+   document.getElementById('menuSlide').classList.remove('menuMobileOpen');
+   document.getElementById('x').classList.remove('xClose');
+   document.getElementById('y').classList.remove('yClose');
+   document.getElementById('z').classList.remove('zClose');
+   document.getElementById('background').classList.remove('backgroundOn');
+   document.getElementById('menuSlide').classList.add("slideMenuOpen");
+   console.log("Yeah, the close is just as smooth.");
+ }
+ });
